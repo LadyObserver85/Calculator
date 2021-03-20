aa381b6cf77d044ef21fa2537d28ee3dffe01e27
Nikolett Muller <nm225@njit.edu>
2021-03-20 00:10:27 -0400

    Feature: Setup calculator with multiplication method and test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -21,6 +21,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.subtract(2, 2), 0)
         self.assertEqual(self.calculator.result, 0)
 
+    def test_multiply_method_calculator(self):
+        self.assertEqual(self.calculator.multiply(2, 5), 10)
+        self.assertEqual(self.calculator.result, 10)
+
 
 if __name__ == '__main__':
     unittest.main()
